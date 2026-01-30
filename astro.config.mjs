2b2d3b1381ad3444eb1e081230da241344368b91
Evan Davison <evan.davison@supdevinci-edu.fr>
2026-01-30 12:30:58 +0100

    ajout de la sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
   site: 'https://evandavison.fr',
@@ -8,5 +9,6 @@ export default defineConfig({
   integrations: [
     react(),
     tailwind(),
+    sitemap(),
   ],
 })
